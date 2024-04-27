8f19534c24614dbeca718d9a8076cdab7caaf6ed
Ignat <petrov-ignat85@mail.ru>
2024-04-27 14:37:43 +0300

    Merge drive_loader with parser

--- app/api.py
+++ app/api.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
 import uvicorn
+from drive_loader import DriveReader
+from parser import parse
 
 app = FastAPI()
 
 
 @app.get("/{dir_id}")
 async def root(dir_id: str):
-    return {"message": "Hello World"}
+    reader = DriveReader()
+    print("File fetching started")
+    transcripts = reader.read_transcripts(dir_id)
+    print("File parsing started")
+    dashboard = parse(transcripts)
+    print(dashboard)
 
 
 if __name__ == "__main__":
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,36 +1,17 @@
 import pandas as pd
-from unittest.mock import mock_open, patch
-from app.parser import read_data, concat_tables, grouping_by_stat, constrain_dashboard, calc_total, calc_summary, EXPECTED_STATES
-
-
-def test_read_data(monkeypatch):
-    CSV_DATA = """start,end,status,name,text
-    0:00:15,0:00:18,Recorded,testname,"text"
-    0:00:19,0:00:21,Recorded,testname,"text"
-    0:00:22,0:00:26,Recorded,testname,text"""
-
-    with patch("builtins.open", mock_open(read_data=CSV_DATA)):
-        files = ["dummy.csv", "dummy2.csv"]
-        result = read_data(files)
-        assert len(result) == 2
-        assert all(isinstance(df, pd.DataFrame) for df in result)
-        assert result[0].shape == (3, 6)
-        assert result[0]["table"][0] == "dummy"
-        assert result[1]["table"][0] == "dummy2"
+from app.parser import concat_tables, grouping_by_stat, constrain_dashboard, calc_total, calc_summary, EXPECTED_STATES
 
 
 def test_concat_tables():
     df1 = pd.DataFrame({
         'name': ['name1', 'name2', 'EXTRA1'],
-        'status': ['Recorded', 'Cleaned', 'Recorded'],
-        'table': ['table1', 'table1', 'table1']
+        'state': ['Recorded', 'Cleaned', 'Recorded']
     })
     df2 = pd.DataFrame({
         'name': ['name2', 'name3', 'EXTRA2'],
-        'status': ['Not recorded', 'Recorded', 'Recorded'],
-        'table': ['table2', 'table2', 'table2']
+        'state': ['Not recorded', 'Recorded', 'Recorded']
     })
-    dataframes = [df1, df2]
+    dataframes = {'table1.csv': df1, 'table2.csv': df2}
     result = concat_tables(dataframes)
     assert 'name' in result.columns
     assert 'status' in result.columns
@@ -42,7 +23,7 @@ def test_grouping_by_stat():
     df = pd.DataFrame({
         'name': ['name2', 'name2', 'name2', 'name3'],
         'table': ['table1', 'table1', 'table2', 'table2'],
-        'status': ['Cleaned', 'Cleaned', 'Not recorded', 'Not recorded']
+        'state': ['Cleaned', 'Cleaned', 'Not recorded', 'Not recorded']
     })
     result = grouping_by_stat(df)
     for state in EXPECTED_STATES:
